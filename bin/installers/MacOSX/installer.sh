68525a3b9be8b61a8df1cb2b83d54972ed4927c8
Ram Ranganathan <smitram@gmail.com>
2013-06-30 23:10:31 -0700

    Some fixes/cleanup to the installer code.

--- bin/installers/MacOSX/installer.sh
+++ bin/installers/MacOSX/installer.sh
@@ -5,40 +5,23 @@ INSTALL_DIR=$(cd -P -- "$SCRIPT_DIR/.." && pwd -P)
 source "$INSTALL_DIR/../../lib/utils"
 
 #  Detect platform and set variables.
-detect_platform
+detect_platform > /dev/null
 
-platform=$STRONGLOOP_INSTALL_PLATFORM
-arch=$STRONGLOOP_INSTALL_ARCH
+if extract_package_files; then
+  setup_configuration "$OCI_DIR"
+  cat << MYEOF
 
-echo "  - StrongLoop Node Oracle $platform installer"
-echo "  - Architecture = $arch"
+---------------------------------------------------------------------------
+The node-oracle module and the Oracle specific libraries have been
+installed in `pwd`. 
 
-#  Downloadable gzipped tarball.
-targz="${ASTEROID_ORACLE_BASE_PACKAGE}_${platform}_${ASTEROID_ORACLE_INSTALL_VERSION}_${arch}.tar.gz"
-
-#  Install to node modules directory.
-nmdir="../node_modules"
-[ -d "$nmdir" ] ||  nmdir="`pwd`/node_modules"
-
-mkdir -p "$nmdir"
-
-pushd "$nmdir" > /dev/null
-echo " - Downloading $ASTEROID_DOWNLOAD_URI/$targz ..."
-curl -o ./$targz  $ASTEROID_DOWNLOAD_URI/$targz
-
-echo " - Extracting $ASTEROID_DOWNLOAD_URI/$targz ..."
-tar -zxf $targz
-
-instantclient_dir="$(pwd)/instantclient/"
-popd > /dev/null
-
-cat << MYEOF
-
-The Node oracle module + the Oracle specific libraries have been installed
-in `pwd`.  Please remember to set the DYLD_LIBRARY_PATH prior to using node.
+The default bashrc (/etc/bashrc) or  user's bash_profile (~/.bash_profile)
+paths have been modified to use this path. If you use a shell other than
+bash, please remember to set the DYLD_LIBRARY_PATH prior to using node.
 
 Example:
-  export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$instantclient_dir"
+  \$ export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:$OCI_DIR"
 
 MYEOF
+fi
 
