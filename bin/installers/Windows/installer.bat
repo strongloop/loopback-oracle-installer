47c0d4a0cbf4b6326bac62c5da6e802714f861d3
Raymond Feng <raymond@strongloop.com>
2013-08-16 16:03:07 -0700

    Cover the case where PATH doesn't exist

--- bin/installers/Windows/installer.bat
+++ bin/installers/Windows/installer.bat
@@ -8,10 +8,11 @@ if exist ../../../../instantclient (
 )
 set ORAPATH=%CD%\vc11;%CD%
 @echo Oracle Path: %ORAPATH%
+set NEWPATH=%ORAPATH%
 
 for /F "tokens=3" %%i in ('reg query "HKEY_CURRENT_USER\Environment" /v PATH') do (
-set NEWPATH=%ORAPATH%;%%i
+  set NEWPATH=%ORAPATH%;%%i
+)
 @echo New Path: %NEWPATH%
 reg add HKEY_CURRENT_USER\Environment /v PATH /d "%NEWPATH%" /f
-)
 popd
