bb314efd8a99d4a46d0244b75f417e60f0553f60
Raymond Feng <rfeng@us.ibm.com>
2016-04-05 12:28:38 -0700

    Fix the installer for npm3

--- pkginstall.js
+++ pkginstall.js
@@ -11,24 +11,25 @@ var version = null;
 
 if (!dest) {
   // Check if it is within the loopback-connector-oracle node_modules
-  var parent = path.join(__dirname, '../../node_modules');
-  if (fs.existsSync(parent)) {
-    dest = parent;
-    try {
-      var pkg = require('../../package.json');  // The parent module
-      if (pkg.config && pkg.config.oracleUrl) {
-        // Allow env var to override config.oracleUrl
-        url = url || pkg.config.oracleUrl;
-        version = pkg.config.oracleVersion;
-      }
-    } catch (err) {
-      // Ignore
+  var connectorPath;
+  try {
+    var connector = require.resolve('loopback-connector-oracle');
+    connectorPath = path.dirname(connector);
+    dest = path.normalize(path.join(connectorPath, '..'));
+    // Find the package.json for loopback-connector-oracle
+    var pkg = require(path.join(connectorPath, 'package.json'));
+    if (pkg.config && pkg.config.oracleUrl) {
+      // Allow env var to override config.oracleUrl
+      url = url || pkg.config.oracleUrl;
+      version = pkg.config.oracleVersion;
     }
+  } catch(err) {
+    // Ignore
   }
 }
 
 // First download the archive
-download(url, version, dest, function (err, result) {
+download(url, version, dest, function(err, result) {
   if (err) {
     process.exit(1);
   }
@@ -37,11 +38,16 @@ download(url, version, dest, function (err, result) {
   var inst_dir = path.dirname(process.argv[1]);
 
   var installer = path.join(inst_dir, 'bin/installers', info.platform, 'installer.sh');
-  // First check the child module
-  var icdir = path.join(inst_dir, 'node_modules/instantclient');
-  if (!fs.existsSync(icdir)) {
-    // Now the peer module
-    icdir = path.join(inst_dir, '../instantclient');
+  var icdir;
+  if (dest) {
+    icdir = path.join(dest, 'instantclient');
+  } else {
+    // First check the child module
+    icdir = path.join(inst_dir, 'node_modules/instantclient');
+    if (!fs.existsSync(icdir)) {
+      // Now the peer module
+      icdir = path.join(inst_dir, '../instantclient');
+    }
   }
 
   var args = [ installer, icdir ];
